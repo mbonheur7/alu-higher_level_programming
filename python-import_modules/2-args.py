dcd1a9f830f12ce9aad7e24cecb76b7cca9786fd
mbonheur7 <m.bonheur1@alustudent.com>
2026-06-26 19:11:31 +0200

    Update 2-args.py

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
 from sys import argv
 
-count = len(argv) - 1
+if __name__ == "__main__":
+    count = len(argv) - 1
 
-if count == 0:
-    print("0 arguments.")
-elif count == 1:
-    print("1 argument:")
-else:
-    print(f"{count} arguments:")
+    if count == 0:
+        print("0 arguments.")
+    elif count == 1:
+        print("1 argument:")
+    else:
+        print(f"{count} arguments:")
 
-for i in range(1, len(argv)):
-    print(f"{i}: {argv[i]}")
+    for i in range(1, len(argv)):
+        print(f"{i}: {argv[i]}")
