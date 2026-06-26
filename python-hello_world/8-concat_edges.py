943a2868bff4557fc1e2a484e0fdc9e375d7af3a
mbonheur7 <m.bonheur1@alustudent.com>
2026-06-26 17:05:24 +0200

    Update 8-concat_edges.py

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming language that combines remarkable power with very clear syntax"
-print(str[39:66] + " with " + str[:6])
+print(str[39:66] + str[76:82] + str[:6])
