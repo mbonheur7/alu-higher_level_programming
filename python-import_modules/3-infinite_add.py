baf7d3f0d3482a224b0f7f25d1e329293aa75f52
mbonheur7 <m.bonheur1@alustudent.com>
2026-06-26 19:15:44 +0200

    Update 3-infinite_add.py

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 from sys import argv
 
-total = 0
+if __name__ == "__main__":
+    total = 0
 
-for i in range(1, len(argv)):
-    total += int(argv[i])
+    for i in range(1, len(argv)):
+        total += int(argv[i])
 
-print(total)
+    print(total)
