60d292ab173d31f0716f056309415e319b4f04b8
mbonheur7 <m.bonheur1@alustudent.com>
2026-06-26 18:58:23 +0200

    alle

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+from sys import argv
+
+count = len(argv) - 1
+
+if count == 0:
+    print("0 arguments.")
+elif count == 1:
+    print("1 argument:")
+else:
+    print(f"{count} arguments:")
+
+for i in range(1, len(argv)):
+    print(f"{i}: {argv[i]}")
---
+++ python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+import importlib.util
+import sys
+
+if __name__ == "__main__":
+    spec = importlib.util.spec_from_file_location("hidden_4", "./hidden_4.pyc")
+    module = importlib.util.module_from_spec(spec)
+    spec.loader.exec_module(module)
+
+    for name in sorted(dir(module)):
+        if not name.startswith("__"):
+            print(name)
