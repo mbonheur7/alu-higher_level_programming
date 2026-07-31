7df2f3c78ddb877b0323fde2556d819fe6ada8cc
mbonheur7 <m.bonheur1@alustudent.com>
2026-07-31 20:16:03 +0200

    Update 1

--- python-network_0/1-body.sh
+++ python-network_0/1-body.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
-# Sends a GET request to a URL and displays the body only if status is 200
-response=$(curl -s -w "\n%{http_code}" "$1")
-status_code=$(echo "$response" | tail -n1)
-body=$(echo "$response" | sed '$ d')
-
-if [ "$status_code" -eq 200 ]; then
-    echo "$body"
-fi
+response=$(curl -s -w "%{http_code}" "$1")
+[ "${response: -3}" = "200" ] && echo "${response::-3}"
